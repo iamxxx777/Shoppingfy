5bfa8d00657b13bf267f634b9de4075ae266ca61
iamxxx777 <iamxxx333@gmail.com>
2021-11-19 15:06:02 +0100

    server editz

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(dirname, '/client/build')));
 
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(dirname, 'client', 'build', 'index.html'));
   });
 }
 
