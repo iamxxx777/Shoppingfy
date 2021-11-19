86974b69eb0a6ea28611f2499004775a10309c6d
iamxxx777 <iamxxx333@gmail.com>
2021-11-19 14:25:34 +0100

    server edits

--- server/server.js
+++ server/server.js
@@ -19,6 +19,9 @@ const dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(dirname, '/client/build')));
 
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use("/api/items", itemsRoute);
