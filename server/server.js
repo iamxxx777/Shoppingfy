d6e990fe261577f7176ce4de8350c6d13aa5200d
iamxxx777 <iamxxx333@gmail.com>
2021-11-20 21:28:42 +0100

    second commit

--- server/server.js
+++ server/server.js
@@ -14,13 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const __dirname = path.resolve();
+const __currentDirectory = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__currentDirectory, '/client/build')));
 
-  app.get('/*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__currentDirectory, 'client', 'build', 'index.html'));
   });
 }
 
