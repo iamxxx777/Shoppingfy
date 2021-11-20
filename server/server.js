5ab5946a859937bc67035d5eb3416fe2e9a5dbf4
iamxxx777 <iamxxx333@gmail.com>
2021-11-20 21:00:12 +0100

    fixing client route

--- server/server.js
+++ server/server.js
@@ -14,13 +14,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const dirname = path.resolve();
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   app.get('/*', function (req, res) {
-    res.sendFile(path.resolve(dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
