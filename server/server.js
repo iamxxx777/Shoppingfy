fb69bbde99d18d1bb5e239ef40a68a404eaa4610
iamxxx777 <iamxxx333@gmail.com>
2021-11-20 20:47:39 +0100

    fixing routing issues

--- server/server.js
+++ server/server.js
@@ -14,10 +14,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const dirname = path.resolve();
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use("/api/items", itemsRoute);
